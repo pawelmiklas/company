dfcb0f6945d171e280f09df3795f5504476e3107
vapaus <snaqerou@gmail.com>
2021-03-22 22:56:04 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
+import { Spin } from "antd";
 import LayoutPage from "components/LayoutPage/LayoutPage";
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import DashboardPage from "pages/DashboardPage/DashboardPage";
+import WorkspacesPage from "pages/WorkspacesPage/WorkspacesPage";
+import React, { Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <LayoutPage />
+        <Suspense fallback={<Spin className="spin" />}>
+          <LayoutPage>
+            <Switch>
+              <Route path="/dashboard" component={DashboardPage} />
+              <Route path="/entities" component={() => <div></div>} />
+              <Route path="/publications" component={() => <div></div>} />
+              <Route path="/workspace/:id" component={WorkspacesPage} />
+              <Redirect to="/dashboard" />
+            </Switch>
+          </LayoutPage>
+        </Suspense>
       </BrowserRouter>
     </QueryClientProvider>
   );
