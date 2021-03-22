308bf20ec1f6c6a81df40fad1600692eac46e653
vapaus <snaqerou@gmail.com>
2021-03-23 00:43:06 +0100

    entities page

--- src/pages/EntitiesPage/EntitiesPage.tsx
+++ src/pages/EntitiesPage/EntitiesPage.tsx
@@ -1,9 +1,33 @@
+import { Card, Col, Row, Typography } from "antd";
 import React from "react";
+import "./EntitiesPage.css";
 
-interface Props {}
+const { Title, Text } = Typography;
 
-const EntitiesPage = (props: Props) => {
-  return <div></div>;
+const EntitiesPage = () => {
+  return (
+    <div className="entitiesWrapper">
+      <Row gutter={16}>
+        <Col span={24}></Col>
+      </Row>
+      <Row>
+        <Col span={6}>
+          <Card bordered={false} style={{ width: "100%" }}>
+            <Row gutter={16}>
+              <Col>{/* <img alt="" /> */}</Col>
+              <Col>
+                <Title level={5}>test</Title>
+                <Text>
+                  Ullamco id ex dolore eu nisi consequat. Enim ea veniam irure
+                  enim tempor et. Ea nulla ex fugiat voluptate id
+                </Text>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default EntitiesPage;
