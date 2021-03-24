dffa4fc4ae4aa530f15f4c9ae1eb2330e96d88d9
vapaus <snaqerou@gmail.com>
2021-03-25 00:01:06 +0100

    dashboard page refator

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -4,16 +4,12 @@ import ResumeList from "./components/ResumeList/ResumeList";
 import WorkspacesSlider from "./components/WorkspacesSlider/WorkspacesSlider";
 import "./DashboardPage.css";
 
-interface Props {}
-
-const DashboardPage = (props: Props) => {
-  return (
-    <div className="dashboardWrapper">
-      <Publications />
-      <WorkspacesSlider />
-      <ResumeList />
-    </div>
-  );
-};
+const DashboardPage = () => (
+  <div className="dashboardWrapper">
+    <Publications />
+    <WorkspacesSlider />
+    <ResumeList />
+  </div>
+);
 
 export default DashboardPage;
